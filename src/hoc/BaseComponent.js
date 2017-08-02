1a7bb7965c18394e5ae45dc9ced3c0067c8f1087
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-08-02 18:05:24 +0530

    Added error logging basic in hoc

--- src/hoc/BaseComponent.js
+++ src/hoc/BaseComponent.js
@@ -5,6 +5,41 @@ const baseHOC = (WrappedComponent) => {
         constructor() {
             super();
             console.log("BaseHoc: ctor");
+            this.registerErrorLog();
+        }
+
+        registerErrorLog() {
+            window.onerror = (message, file, line, column, errorObject) => {
+                column = column || (window.event && window.event.errorCharacter);
+                var stack = errorObject ? errorObject.stack : null;
+
+            //trying to get stack from IE
+            if(!stack)
+            {
+                var stack = [];
+                    var f = arguments.callee.caller;
+                    while (f)
+                    {
+                        stack.push(f.name);
+                        f = f.caller;
+                    }
+                    errorObject['stack'] = stack;
+                }
+
+                var data = {
+                    message:message,
+                    file:file,
+                    line:line,
+                    column:column,
+                    errorStack:stack,
+                };
+
+                //here I make a call to the server to log the error
+
+                //the error can still be triggered as usual, we just wanted to know what's happening on the client side
+                console.log(data);
+                return false;
+            }
         }
         render() {
             return <WrappedComponent {...this.props}/>
